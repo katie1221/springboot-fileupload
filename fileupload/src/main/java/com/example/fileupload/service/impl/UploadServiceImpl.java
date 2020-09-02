306049332cc2a7c364ada942148ef101b18bb944
katie1221 <68039943+katie1221@users.noreply.github.com>
2020-09-02 14:24:14 +0800

    新增 项目目录下的图片文件上传

--- fileupload/src/main/java/com/example/fileupload/service/impl/UploadServiceImpl.java
+++ fileupload/src/main/java/com/example/fileupload/service/impl/UploadServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.fileupload.service.impl;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.*;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.fileupload.service.UploadService;
@@ -94,5 +98,55 @@ public class UploadServiceImpl implements UploadService{
 		resultMap.put("imgUrl", imgUrl);
 		resultMap.put("returnCode", 200);
 		return resultMap;
+	}
+	/**
+	 * 项目目录下的图片文件上传
+	 */
+	@Override
+	public Map<String, Object> getImageUrl(HttpServletRequest request) throws Exception {
+		Map<String,Object> result = new HashMap<String,Object>();
+		//获取图片在项目路径下的地址
+		String basePath=ClassUtils.getDefaultClassLoader().getResource("").getPath();
+		String oldPath=basePath+"/static/images/animate.png";
+		
+		Long res =new Date().getTime();
+		//设置文件存储路径，可以存放在你想要指定的路径里面
+		String rootPath="D:/mimi/"+File.separator+"upload/images/";
+		// 新文件名
+		String newFileName =res + oldPath.substring(oldPath.lastIndexOf("."));
+			//新文件
+		File newFile=new File(rootPath+File.separator+newFileName);
+		//判断文件目录是否存在
+		if(!newFile.getParentFile().exists()){
+			//如果目标文件所在的目录不存在，则创建父目录
+			newFile.getParentFile().mkdirs();
+		}
+        
+		//-------把图片文件写入磁盘 start ----------------
+		FileOutputStream fos = new FileOutputStream(newFile);
+		//读取本地文件
+		File localFile = new File(oldPath);
+		//获取本地文件输入流
+		InputStream stream=new FileInputStream(localFile);
+		//写入目标文件
+		byte[] buffer=new byte[1024*1024];
+		int byteRead=0;
+		//stream.read(buffer) 每次读到的数据存放在 buffer 数组中
+		while((byteRead=stream.read(buffer))!=-1){
+			//在 buffer 数组中 取出数据 写到 （输出流）磁盘上
+			fos.write(buffer, 0, byteRead);
+			fos.flush();
+		}
+		fos.close();
+		stream.close();
+		//-------把图片文件写入磁盘 end ----------------
+        
+		//服务器图片地址
+		String baseURL = request.getScheme() + "://" + request.getServerName()
+		+ ":" + request.getServerPort()+"/mimi/upload/images/";
+		String imgUrl=baseURL+newFileName;
+
+		result.put("imgUrl", imgUrl);
+		return result;
 	}
 }
